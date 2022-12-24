32c38faa8c220de59c109efed325aa31d3831e14
unknown <sifatniloy18@gmail.com>
2022-12-24 11:41:35 -0600

    typescript file setup complete

---
+++ dist/app.js
@@ -0,0 +1,4 @@
+"use strict";
+var message = "Hello Typescript";
+var message2 = "Tata Javascript";
+console.log(message, message2, language);
---
+++ src/app.ts
@@ -0,0 +1,3 @@
+const message= "Hello Typescript";
+const message2= "Tata Javascript";
+console.log(message, message2, language);
