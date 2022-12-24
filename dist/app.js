307605c23b3e692dadb15a5c922c93bc0e022a0a
unknown <sifatniloy18@gmail.com>
2022-12-24 12:46:43 -0600

    type handleing

--- dist/app.js
+++ dist/app.js
@@ -2,3 +2,8 @@
 var message = "Hello Typescript";
 var message2 = "Tata Javascript";
 console.log(message, message2, language);
+// let myName: string;
+// myName="Niloy"
+var age = 24;
+age = "25";
+var myName = "Sifat";
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 const message= "Hello Typescript";
 const message2= "Tata Javascript";
 console.log(message, message2, language);
+
+// let myName: string;
+// myName="Niloy"
+
+let age: number| string | boolean | [] =24;
+age="25"
+
+let myName: "Sifat" ="Sifat";
